04b0d6448748e735f7634d016c9331e3b02b461e
nkey44 <126723210+nkey44@users.noreply.github.com>
2023-03-06 14:11:58 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-streamlit.stop()
 
 # create the repeatable code block (called a function)
 def get_fruityvice_data(this_fruit_choice):
